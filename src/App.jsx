df54696806b6aa36fb83a93b94d3440df1fc90f1
indiphilegogela <156888615+indiphilegogela@users.noreply.github.com>
2025-06-08 23:26:17 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import React from "react"
 import Header from "./components/Header"
 import Meme from "./components/Meme"
 
-
 export default function App() {
   return (
-      <div>
-          <h1></h1>
-          <Header/>
-          <Meme/>
-      </div>
+    <>
+      <Header />
+      <Meme />
+    </>
   )
 }
